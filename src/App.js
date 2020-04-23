836a1fdfa8a94ef5b7f858a63aea79458b7fbe90
edmad <edmadx25@gmail.com>
2020-04-22 23:14:05 -0500

    login y registro agragado

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          vamos a escuchar musica en cautivaradio!
+ cautivaradio.com
         </a>
       </header>
     </div>
